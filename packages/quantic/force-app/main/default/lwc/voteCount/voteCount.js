6ec0fe83abaca36c98d681939df5a093ceb55c60
MehdiMitiche <gm_mitiche@esi.dz>
2021-11-11 23:19:33 -0500

    SFINT-4130 voteCount component added

--- packages/quantic/force-app/main/default/lwc/voteCount/voteCount.js
+++ packages/quantic/force-app/main/default/lwc/voteCount/voteCount.js
@@ -1,3 +1,44 @@
-import { LightningElement } from 'lwc';
+import { LightningElement, api } from 'lwc';
+import helpedUsers from '@salesforce/label/c.cookbook_HelpedUsers';
+import helpedUsers_plural from '@salesforce/label/c.cookbook_HelpedUsers_plural';
+import helpedUsers_zero from '@salesforce/label/c.cookbook_HelpedUsers_zero';
+import { I18nUtils } from 'c/quanticUtils';
 
-export default class VoteCount extends LightningElement {}
+
+
+/**
+ * The `voteCount` component is an indicator of how many people said that a document was helpful. 
+ * @example
+ * <c-vote-count></c-vote-count>
+ */
+export default class VoteCount extends LightningElement {
+  labels = {
+    helpedUsers,
+    helpedUsers_plural,
+    helpedUsers_zero
+  }
+
+  /**
+   * The count to be shown to the user.
+   * @api
+   * @type {number}
+   * @defaultValue `0`
+   */
+  @api count = 0;
+
+  /**
+   * The alternative text to be assigned to the button icon.
+   * @api
+   * @type {string}
+   */
+  @api altText;
+
+  /**
+   * Returns the label to show with the count value.c/descriptionStrengthIndicator
+   * @returns {string}
+   */
+  get label() {
+    const labelName = I18nUtils.getLabelNameWithCount('helpedUsers', this.count)
+    return I18nUtils.format(this.labels[labelName], this.count);
+  }
+}
